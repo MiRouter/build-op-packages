fb53789d6d88ced3a38bb9612e7fc0f4ff78143f
liuran001 <32791471+liuran001@users.noreply.github.com>
2020-08-08 10:28:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,17 +12,17 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-cd package
+cd package/feeds/opapps
 git clone https://github.com/jerrykuku/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr
 git clone https://github.com/Lienol/openwrt-package
 git clone https://github.com/tty228/luci-app-serverchan
 git clone https://github.com/cnzd/luci-app-koolproxyR
-cd lean
+cd -
+cd package/lean
 rm -rf luci-theme-argon  
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git 
-cd -
-
+cd ..
 mkdir -p luci-app-diskman && \
 mkdir -p parted && \
 wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Makefile -O luci-app-diskman/Makefile
